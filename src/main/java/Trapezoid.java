562462fb898f9fbc75b53ea0713f7e9226d591cb
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-07-29 16:24:47 +0300

    changed commented description under heightLength method of a Trapezoid class

--- src/main/java/Trapezoid.java
+++ src/main/java/Trapezoid.java
@@ -10,8 +10,8 @@ public class Trapezoid extends GeometricObject {
     }
 
     public double heightLength() {
-        // since we don't know the length of the sides, let's assume that we're dealing with isosceles trapezoid,
-        // whose cathetus are equal
+        // since we don't know the length of the sides, let's assume that we're dealing with
+        // idealized isosceles trapezoid, where b = 3*a and h = a;
         double result = Math.sqrt(2) * Math.sqrt(this.area);
         return result;
     }

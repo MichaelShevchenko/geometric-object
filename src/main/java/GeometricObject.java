caccc5265d42dd3e898f39228b4190f51b5428ed
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-07-29 16:26:24 +0300

    deleted commented lines in GeometricObject class

--- src/main/java/GeometricObject.java
+++ src/main/java/GeometricObject.java
@@ -1,8 +1,3 @@
 public abstract class GeometricObject {
     public abstract void getAttributes();
-    //public abstract void draw();
-
-    // public abstract void setArea();
-
-    // public abstract void setColor();
 }
